016d42c444429298d4feac3341c218fc19996850
Max Bane <max@thebanes.org>
2022-11-29 14:25:32 +0000

    Module boilerplate.

--- src/frankenfit/mypy.py
+++ src/frankenfit/mypy.py
@@ -1,3 +1,38 @@
+# Unpublished Copyright (c) 2022 Max Bane, all rights reserved.
+#
+# NOTICE: All information contained herein is, and remains the property of Max Bane.
+# The intellectual and technical concepts contained herein are proprietary to Max Bane
+# and may be covered by U.S. and Foreign Patents, patents in process, and are protected
+# by trade secret or copyright law. Dissemination of this information or reproduction
+# of this material is strictly forbidden unless prior written permission is obtained
+# from Max Bane. Access to the source code contained herein is hereby forbidden to
+# anyone except current employees, contractors, or customers of Max Bane who have
+# executed Confidentiality and Non-disclosure agreements explicitly covering such
+# access.
+#
+# The copyright notice above does not evidence any actual or intended publication or
+# disclosure of this source code, which includes information that is confidential
+# and/or proprietary, and is a trade secret, of Max Bane. ANY REPRODUCTION,
+# MODIFICATION, DISTRIBUTION, PUBLIC PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE
+# OF THIS SOURCE CODE WITHOUT THE EXPRESS WRITTEN CONSENT OF MAX BANE IS STRICTLY
+# PROHIBITED, AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE
+# RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY
+# OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, NOR TO
+# MANUFACTURE, USE, OR SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+
+"""
+A Mypy plugin for use when typechecking code that uses the frankenfit library.
+Piggy-backs on the built-in attrs plugin to make Mypy aware of the automagic
+that the @param decorator does, and also expands the constructor signatures of
+Transform subclasses to allow hyperparameters (`HP` instances) for all
+parameters, in addition to their annotated types.
+
+Example mypy config in `pyroject.toml`::
+
+    [tool.mypy]
+    plugins = "frankenfit.mypy"
+
+"""
 from __future__ import annotations
 from typing import Callable, cast
 from warnings import warn
